baa5f3b4bb170adf30b07099cbeddcbf6c40f80f
shatha <shatthasultann9@gmail.com>
2024-09-29 11:11:03 +0300

    Initial Commit

---
+++ Ghithubshatha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ghithubshatha
+//
+//  Created by shatha alsawilam on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ghithubshatha/GhithubshathaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhithubshathaApp.swift
+//  Ghithubshatha
+//
+//  Created by shatha alsawilam on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct GhithubshathaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

6b5471a6b3a4a6fe203ffc58cc88c5703124fd34
shatha <shatthasultann9@gmail.com>
2024-09-30 20:05:25 +0300

    splash

--- Ghithubshatha/ContentView.swift
+++ Ghithubshatha/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showImage = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if showImage {
+                Image("MandoImage") // استدعاء الصورة من الـ Assets
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .transition(.opacity) // تأثير الظهور والاختفاء
+                    .animation(.easeInOut(duration: 0.4), value: showImage)
+                
+                Text("Mando")
+                    .font(.largeTitle)
+                    .foregroundColor(Color("BlueColor")) // استدعاء اللون من الـ Assets
+                    .transition(.opacity)
+                    .animation(.easeInOut(duration: 0.4), value: showImage)
+            }
         }
-        .padding()
+        .onAppear {
+            withAnimation {
+                self.showImage = true
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                withAnimation {
+                    self.showImage = false
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
